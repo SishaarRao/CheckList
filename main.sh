6552539366face4e8eaf557142fd4fd51b6797ae
Sishaar Rao <sishaar@gmail.com>
2017-03-21 10:38:08 -0400

    passing parameters to the function

--- main.sh
+++ main.sh
@@ -12,6 +12,7 @@
 KEYWORDS=("init" "list" "add" "remove" "complete")
 
 __tokenize (){
+
     # Verify that param isn't empty
     if [[ -z $*  ]]; then
 	echo "Error Code 4: No command provided"
@@ -21,41 +22,47 @@ __tokenize (){
     command=""
     # Parse through params, get command
     for param in $*; do
-#	if [[ "$command" != "" ]] && [[ " ${KEYWORDS[*]} " == *" $param "* ]]; then
-#	    echo "Error Code 3: Too many keywords in input"
-#	    exit 3
-#	fi
-	if [[ "$command" == "" ]] && [[ "${KEYWORDS[*]}" == *" $param "* ]]; then
+	if [[ "$command" != "" ]] && [[ " ${KEYWORDS[*]} " == *" $param "* ]]; then
+	    echo "Error Code 3: Too many keywords in input"
+	    exit 3
+	fi
+	if [[ "$command" == "" ]] && [[ " ${KEYWORDS[*]} " == *" $param "* ]]; then
 	    command="$param"
 	    break
 	fi
     done
 
-    ${command}
+    ${command} ${@:2}
 }
+
 
 init (){
     echo "This is the function for initializing a new list"
+    echo "Params:" $*
     exit 0
 }
 
 list (){
     echo "This is the function for listing the contents of a list"
+    echo "Params:" $*
     exit 0
 }
 
 add (){
     echo "This is the function for adding an element to a list"
+    echo "Params:" $*
     exit 0
 }
 
 remove (){
     echo "This is the function for removing an element from a list"
+    echo "Params:" $*
     exit 0
 }
 
 complete (){
     echo "This is the function for marking a task complete on a list"
+    echo "Params:" $*
     exit 0
 }
 
