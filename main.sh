3f7630c9c4fdabbcaf4d69a909d41fb6224fbd6c
Sishaar Rao <sishaar@gmail.com>
2017-03-21 10:12:04 -0400

    Parsing commands, turns out we don't care about multiple commands, we only care about the first one, so I guess init add is okay, but it's slightly annoying

---
+++ main.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+#######################################
+#    Created by Sishaar Rao
+#    This is the Version 1.0 
+#    implementation of Checklist 
+#######################################
+
+# Add Dependencies
+. ./.dependencies/ticktick.sh
+
+
+KEYWORDS=("init" "list" "add" "remove" "complete")
+
+__tokenize (){
+    # Verify that param isn't empty
+    if [[ -z $*  ]]; then
+	echo "Error Code 4: No command provided"
+	exit 4
+    fi
+    
+    command=""
+    # Parse through params, get command
+    for param in $*; do
+#	if [[ "$command" != "" ]] && [[ " ${KEYWORDS[*]} " == *" $param "* ]]; then
+#	    echo "Error Code 3: Too many keywords in input"
+#	    exit 3
+#	fi
+	if [[ "$command" == "" ]] && [[ "${KEYWORDS[*]}" == *" $param "* ]]; then
+	    command="$param"
+	    break
+	fi
+    done
+
+    ${command}
+}
+
+init (){
+    echo "This is the function for initializing a new list"
+    exit 0
+}
+
+list (){
+    echo "This is the function for listing the contents of a list"
+    exit 0
+}
+
+add (){
+    echo "This is the function for adding an element to a list"
+    exit 0
+}
+
+remove (){
+    echo "This is the function for removing an element from a list"
+    exit 0
+}
+
+complete (){
+    echo "This is the function for marking a task complete on a list"
+    exit 0
+}
+
+
+# Tokenize input, call respective function
+__tokenize $*
+
